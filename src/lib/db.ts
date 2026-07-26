34e32d83db0668a95734047e33f9df8570c99a56
Z User <z@container>
2026-07-26 12:04:13 +0000

    Switch to new Neon PostgreSQL connection string

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,13 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    // Enable connection pooling for serverless (Neon)
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
